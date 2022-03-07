0751633288df1a96c33686a236b744b102f7f74d
Jan von der Assen <vonderassen@ifi.uzh.ch>
2022-03-07 12:02:10 +0100

    table

--- source/components/App.vue
+++ source/components/App.vue
@@ -1,14 +1,64 @@
 <template>
   <div id="front">
+    <vue-good-table
+      :columns="columns"
+      :rows="threats"/>
+
   </div>
 </template>
 
 <script>
+import 'vue-good-table/dist/vue-good-table.css'
+import { VueGoodTable } from 'vue-good-table';
+
+// add to component
 export default {
   name: 'App',
   data: () => ({
-    message: 'coretm-report'
-  })
+    columns: [
+      {
+        label: 'ID',
+        field: 'id'
+      },
+      {
+        label: 'Threat',
+        field: 'label'
+      },
+      {
+        label: 'Description',
+        field: 'description'
+      }
+    ],
+    threats: [
+    ]
+  }),
+  components: {
+    VueGoodTable,
+  },
+  mounted () {
+    var diagram = viewer.getBundleById('com.vonderassen.coretm')
+    var model = diagram.getDizmos()[0]
+    var data = model.publicStorage.getProperty('diagramdecoded')
+    console.log(this)
+    processModel.bind(this)('', data)
+    model.publicStorage.subscribeToProperty('diagramdecoded', processModel.bind(this))
+
+    function processModel (_, changes) {
+      var { data } = changes
+
+      var parser = new DOMParser()
+      var xmlDoc = parser.parseFromString(data, 'text/xml')
+      var threatRecords = [...xmlDoc.querySelectorAll('[coretm-type=threat]')]
+      var threats = threatRecords.map((el) => {
+        return {
+          id: el.id,
+          label: el.getAttribute('label'),
+          description: el.getAttribute('description') || '...'
+        }
+      })
+      this.threats = threats
+    }
+  }
 };
 </script>
 
