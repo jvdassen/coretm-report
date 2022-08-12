9ad4614b45e7b938fc4d9df11b85fecdf3ccd2ff
Jan von der Assen <jan.vda@gmx.ch>
2022-08-12 16:19:18 +0200

    Bug Fixes and Styling

--- source/components/App.vue
+++ source/components/App.vue
@@ -21,13 +21,13 @@ export default {
         field: 'id'
       },
       {
-        label: 'Threat',
+        label: 'Asset',
         field: 'label'
-      },
-      {
+      }
+/*      {
         label: 'Description',
         field: 'description'
-      }
+      }*/
     ],
     threats: [
     ]
@@ -51,18 +51,38 @@ export default {
       window.xmlDoc = xmlDoc
       var threatRecords = [...xmlDoc.querySelectorAll('[coretm-type=threat]')]
       var assetObjects = [...xmlDoc.querySelectorAll('mxCell')].filter(e => e?.attributes['style']?.textContent.includes('E3C800'))
+      console.log(assetObjects)
       var assetCells = assetObjects.map(a => a.parentNode)
       var threats = assetCells.map((el) => {
         return {
           id: el.id,
-          label: el.getAttribute('label'),
+          label: el.getAttribute('label').replaceAll('<br>', ' ').replaceAll('<div>', ' ').replaceAll('</div>', ' '),
           description: el.getAttribute('description') || '...'
         }
       })
+      this.threats = threats
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+table.vgt-table {
+  background-color: #2d2c2c !important;
+  border: #535151 !important;
+}
+
+.vgt-table.bordered td, .vgt-table.bordered th {
+  border: none !important;
+}
+
+.vgt-table thead th {
+  color: #edf3ff !important;
+  background: #3d3d3d !important;
+}
+
+table.vgt-table td {
+  color: #c8cedb !important;
+}
 </style>
+
